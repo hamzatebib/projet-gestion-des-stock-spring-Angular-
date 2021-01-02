eb86692fef1f07e16f4b82e7bb830e041057286a
zeus000 <39542042+zeus000@users.noreply.github.com>
2021-01-02 03:45:39 +0100

    commit4

--- GDSFRONT1/src/app/login/login.component.ts
+++ GDSFRONT1/src/app/login/login.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+ 
+import { Component, OnInit } from '@angular/core'; import { Router, ActivatedRoute} from '@angular/router'; 
+import { AuthenticationService } from '../services/authentication.service';  
+@Component({  
+     selector: 'app-login',  
+      templateUrl: './login.component.html', 
+        styleUrls: ['./login.component.css'] 
+}) export class LoginComponent implements OnInit { 
+ 
+  username: string;  
+   password: string;  
+    invalidLogin = false; 
+     successMessage = "Authentication success";  
+      errorMessage = "Invalide username or password";   
+      constructor(private router: Router, private loginservice: AuthenticationService) { } 
+   ngOnInit() { 
+  }    checkLogin() { 
+    if (this.loginservice.authenticate(this.username, this.password)) {     
+          this.router.navigate(['']) 
+} else       this.invalidLogin = true 
+} 
 
-@Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
-})
-export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+} 
