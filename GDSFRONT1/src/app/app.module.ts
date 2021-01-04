1a4ec44a3d9998e5f73934abee996fd287ba9985
zeus000 <39542042+zeus000@users.noreply.github.com>
2021-01-04 02:20:29 +0100

    Update app.module.ts

--- GDSFRONT1/src/app/app.module.ts
+++ GDSFRONT1/src/app/app.module.ts
@@ -1,36 +1,56 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { LoginComponent } from './login/login.component';
-import { LogoutComponent } from './logout/logout.component';
-import { AddArticleComponent } from './add-article/add-article.component';
-import { AddProviderComponent } from './add-provider/add-provider.component';
 
-import { ListeArticleComponent } from './liste-article/liste-article.component';
-import { UpdateArticleComponent } from './update-article/update-article.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AddProviderComponent } from './add-provider/add-provider.component';
+import { ListProviderComponent } from './list-provider/list-provider.component';
 import { UpdateProviderComponent } from './update-provider/update-provider.component';
+import { LogoutComponent } from './logout/logout.component';
+import { LoginComponent } from './login/login.component';
+
+import { BasicAuthHtppInterceptorServiceService } from './services/basic-auth-htpp-interceptor-service.service';
+
+
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AddArticleComponent } from './add-article/add-article.component';
+import { UpdateArticleComponent } from './update-article/update-article.component';
+import { ListeArticleComponent } from './liste-article/liste-article.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent,
+    
     NavbarComponent,
+    AddProviderComponent,
+    ListProviderComponent,
+    UpdateProviderComponent,
     LoginComponent,
     LogoutComponent,
     AddArticleComponent,
-    AddProviderComponent,
-    
-    ListeArticleComponent,
     UpdateArticleComponent,
-    UpdateProviderComponent
+    ListeArticleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: BasicAuthHtppInterceptorServiceService,
+      multi: true
+      }
+     
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
