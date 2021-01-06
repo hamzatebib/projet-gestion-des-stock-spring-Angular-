bbc1ff7277fad1165f6273695b3f009bd72bb3e9
zeus000 <39542042+zeus000@users.noreply.github.com>
2021-01-06 01:14:36 +0100

    update list provider

--- GDSFRONT1/src/app/list-provider/list-provider.component.ts
+++ GDSFRONT1/src/app/list-provider/list-provider.component.ts
@@ -1,4 +1,7 @@
+import { ProviderService } from './../services/provider.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-list-provider',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListProviderComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  providers: any;
+
+  constructor(private service: ProviderService,  private router: Router) { }
+
+  ngOnInit() {
+    this.service.listProviders().subscribe(
+      response => {
+        this.providers = response;
+      }
+    );
+  }
+
+  deleteProvider(myObj) {
+    //console.log(myObj);
+    this.service.deleteProvider(myObj).subscribe(response => {
+      console.log(response);
+      this.refreshListProviders();
+    })
+  }
+
+  refreshListProviders() {
+    this.service.listProviders().subscribe(
+      response => {
+        this.providers = response;
+      }
+    );
+  }
+
+
+  updateProvider(myObj) {
+    this.router.navigate(['updateProvider' + '/' + myObj['id']]);
   }
 
 }
