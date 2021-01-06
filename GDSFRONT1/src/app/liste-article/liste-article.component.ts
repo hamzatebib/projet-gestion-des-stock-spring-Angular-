7d6d96dcc9e61ce8bdc49105a630ab685d83e827
zeus000 <39542042+zeus000@users.noreply.github.com>
2021-01-06 01:14:58 +0100

    update liste article

--- GDSFRONT1/src/app/liste-article/liste-article.component.ts
+++ GDSFRONT1/src/app/liste-article/liste-article.component.ts
@@ -1,4 +1,6 @@
+import { ArticleService } from './../services/article.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-liste-article',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./liste-article.component.css']
 })
 export class ListeArticleComponent implements OnInit {
+articles:any;
+  constructor(private service: ArticleService,  private router: Router) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit() {
+    this.service.listArticles().subscribe(
+      response => {
+        this.articles = response;
+  });
 
 }
+deleteArticle(myObj) {
+  //console.log(myObj);
+  this.service.deleteArticle(myObj).subscribe(response => {
+    console.log(response);
+    this.refreshListArticles();
+  })
+}
+
+refreshListArticles() {
+  this.service.listArticles().subscribe(
+    response => {
+      this.articles = response;
+    }
+  );
+}
+
+
+updateArticle(myObj) {
+  this.router.navigate(['updateArticle' + '/' + myObj['id']]);
+}
+
+}
+
